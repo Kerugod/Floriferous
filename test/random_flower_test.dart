38752cf59c2a29ebe7f14a1977770f4a11d6d08d
Kelvin <kelvinlol53@gmail.com>
2023-03-28 23:16:30 -0700

    Creando test para los metodos de color y tipo de flor
    Revisando si en mil intentos por cada método se cumple que salen todos los posibles resultados.

--- test/random_flower_test.dart
+++ test/random_flower_test.dart
@@ -2,12 +2,73 @@ import 'package:a/Cards/FlowerCards.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('randoFlowerResult', () {
-    FLowerCards fc = new FLowerCards();
-    expect(fc.color, "Red");
-    expect(fc.color, "Purple");
-    expect(fc.color, "White");
-    expect(fc.color, "Yellow");
-    expect(fc.color, "Violet");
+  group('La generacion de cartas de flor funciona correctamente', () {
+    test('Ver si la asignacion del color esta funcionando', () {
+      bool red = false,
+          violet = false,
+          purple = false,
+          white = false,
+          yellow = false;
+      for (int i = 0; i < 1000; i++) {
+        FLowerCards fc = new FLowerCards();
+        switch (fc.color) {
+          case "Red":
+            red = true;
+            break;
+
+          case "Violet":
+            violet = true;
+            break;
+
+          case "Purple":
+            purple = true;
+            break;
+
+          case "White":
+            white = true;
+            break;
+
+          case "Yellow":
+            yellow = true;
+            break;
+        }
+      }
+
+      expect(red && violet && purple && white && yellow, true);
+    });
+
+    test('Saber si el tipo de flor se está realizando correctamente', () {
+      bool daisy = false,
+          lily = false,
+          mum = false,
+          poppy = false,
+          tulip = false;
+      for (int i = 0; i < 1000; i++) {
+        FLowerCards fc = new FLowerCards();
+        switch (fc.color) {
+          case "Daisy":
+            daisy = true;
+            break;
+
+          case "Lily":
+            lily = true;
+            break;
+
+          case "Mum":
+            mum = true;
+            break;
+
+          case "Poppy":
+            poppy = true;
+            break;
+
+          case "Tulip":
+            tulip = true;
+            break;
+        }
+      }
+
+      expect(daisy && lily && mum && poppy && tulip, true);
+    });
   });
 }
